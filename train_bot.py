cb0e78fbdf72c5871d630ec0d379509be4bdc431
sandhyadav <66108338+sandhyadav@users.noreply.github.com>
2023-06-19 20:10:48 +0530

    Add files via upload

---
+++ train_bot.py
@@ -0,0 +1,26 @@
+#Text Data Preprocessing Lib
+import nltk
+
+from nltk.stem import PorterStemmer
+stemmer = PorterStemmer()
+
+import json
+import pickle
+import numpy as np
+
+words=[]
+classes = []
+word_tags_list = []
+ignore_words = ['?', '!',',','.', "'s", "'m"]
+train_data_file = open('intents.json').read()
+intents = json.loads(train_data_file)
+
+# function for appending stem words
+def get_stem_words(words, ignore_words):
+    stem_words = []
+    for word in words:
+        if word not in ignore_words:
+            w = stemmer.stem(word.lower())
+            stem_words.append(w)  
+    return stem_words
+
